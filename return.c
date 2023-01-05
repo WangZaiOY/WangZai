6361ab5ac6dae6642ca2dc09ecb377ab8a7a60ef
Jiang <JiangOuO@outlook.com>
2023-01-05 18:49:52 +0800

    C Primer plus 3.4.5 编程学习 23/1/5

--- return.c
+++ return.c
@@ -117,3 +117,28 @@ void two()
 {
 	printf("two\n");
 }
+
+
+
+#include <stdio.h>
+
+                  /*C Primer plus 3.4.5 练习*/
+int main(void)
+{
+	float weight;
+	float value;
+
+	printf("Are you worth your weight in platinum?\n");
+	printf("Let's check it out.\n");
+	printf("Please enter your weight in pounds:");
+
+	scanf_s("%f", &weight);
+
+	value = 1700.0 * weight * 14.5833;
+
+	printf("Your weight in platinum is worth $%.2f.\n", value);
+	printf("You are easily worth that! If platinum prices drop,\n");
+	printf("eat more to maintain your value.\n");
+
+	return 0;
+}
